1acbdf1fee426d468a16efb32339cdb06cb25230
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-10-20 20:45:26 +0200

    creat the function gestSentece

---
+++ index.js
@@ -0,0 +1,10 @@
+function getSentence (){
+    let sentence =document.getElementById("sentence").value;
+    return sentence;
+}
+
+function getVocals() {
+    const sentence = getSentence();  // Obtiene la frase
+    
+    return result;
+}
