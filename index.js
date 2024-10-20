cee34b515b9e8e585a8ec713bbc7a72685166bf9
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-10-20 22:28:51 +0200

    creat bonton of repetir frase

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function getSentence (){
-    let sentence =document.getElementById("sentence").value;
-    return sentence;
-}
-
-function getVocals() {
-    const sentence = getSentence();  // Obtiene la frase
-    
-    return result;
+function getSentence() {
+    let sentence = document.getElementById('sentence').value;  
+    document.getElementById('result').textContent = `'${sentence}'`;  
+    return `'${sentence}'`; 
 }
