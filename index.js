476f15b635e9b4aa76abeefcd645085df8b5e08b
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-10-20 23:18:21 +0200

    creat the buscador del vocal

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ function getSentence() {
     document.getElementById('result').textContent = `'${sentence}'`;  
     return `'${sentence}'`; 
 }
+function searchVocals(sentence) {
+    let  vocals = ['a', 'e', 'i', 'o', 'u'];
+    let  foundVocals = [];
+    for (let char of sentence.toLowerCase()) {
+        if (vocals.includes(char)) {
+            foundVocals.push(char);
+        }
+    }
+    return foundVocals;
+}
+
+
+function deleteDuplicity(list) {
+    return [...new Set(list)];
+}
+function getVocals() {
+    const sentence = getSentence();  // Obtiene la frase
+    let foundVocals = searchVocals(sentence);  // Busca vocales en la frase
+    foundVocals = deleteDuplicity(foundVocals);  // Elimina duplicados
+    const result = foundVocals.join(',');  // Junta las vocales en un string
+    document.getElementById('resulto').textContent = result;  // Muestra el resultado
+    return result;
+}
